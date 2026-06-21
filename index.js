6b1b934cb3e61a4b997d088fb9525f9d3a5bfac3
karim-ullah <freelancerkarimullah@gmail.com>
2026-06-22 02:54:08 +0600

    initiated and added doctor and schedule api

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+    const db = client.db("medicare");
+
+    const doctorCollections = db.collection("doctors");
+    const doctorScheduleCollections = db.collection("doctorSchedule");
+
+    // doctors profile or doctorCollections
+
+    app.patch("/api/doctors/:doctorId", async (req, res) => {
+      try {
+        const doctorId = req.params.doctorId;
+        const doctorData = req.body;
+
+        const filter = {
+          doctorId,
+        };
+
+        const updateDoc = {
+          $set: {
+            ...doctorData,
+            updatedAt: new Date(),
+          },
+          $setOnInsert: {
+            createdAt: new Date(),
+            status: 'Pending'
+          },
+        };
+
+        const options = {
+          upsert: true,
+        };
+
+        const result = await doctorCollections.updateOne(
+          filter,
+          updateDoc,
+          options,
+        );
+
+        res.status(200).send({
+          success: true,
+          message: "Doctor profile saved successfully",
+          result,
+        });
+      } catch (error) {
+        console.error(error);
+
+        res.status(500).send({
+          success: false,
+          message: "Something went wrong",
+        });
+      }
+    });
+
+
+
+    app.post("/api/doctor-schedule", async (req, res) => {
+      const data = req.body;
+      const result = await doctorScheduleCollections.insertOne(data)
+      res.send(result)
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
