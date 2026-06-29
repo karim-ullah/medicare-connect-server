65e5f27c8d50fe3cb2318205309db6215aecac67
karim-ullah <freelancerkarimullah@gmail.com>
2026-06-29 19:38:50 +0600

    added api for search and filter function for find-doctors page

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ async function run() {
 
     const users = db.collection("user");
 
+    const paymentCollections = db.collection("payments");
+
     // ---admin area started ---
     app.get("/api/get-users", async (req, res) => {
       const result = await users.find().toArray();
@@ -64,21 +66,17 @@ async function run() {
       res.send(doctorResult, scheduleResult);
     });
 
-
     // suspend user by admin---
-    app.patch('/api/suspend-user', async(req,res)=>{
-      const id = req.query.userId
+    app.patch("/api/suspend-user", async (req, res) => {
+      const id = req.query.userId;
       const filter = {
-        _id: new ObjectId(id)
-      }
-      const result = await users.updateOne(
-        filter,
-        {
-          $set: req.body
-        }
-      )
-      res.send(result)
-    })
+        _id: new ObjectId(id),
+      };
+      const result = await users.updateOne(filter, {
+        $set: req.body,
+      });
+      res.send(result);
+    });
 
     // ---admin area closed ---
 
@@ -139,13 +137,100 @@ async function run() {
       const result = await appointmentCollections.find(filter).toArray();
       res.send(result);
     });
+
+    //delete appointment request from appointments by id
+    app.delete("/api/delete-appointment", async (req, res) => {
+      const id = req.query.appointmentId;
+      const filter = {
+        _id: new ObjectId(id),
+      };
+      const result = await appointmentCollections.deleteOne(filter);
+      res.send(result);
+    });
+
+    app.patch("/api/update-appointment-day", async (req, res) => {
+      const id = req.query.appointmentId;
+      const filter = {
+        _id: new ObjectId(id),
+      };
+
+      const result = await appointmentCollections.updateOne(filter, {
+        $set: req.body,
+      });
+
+      res.send(result);
+    });
+
+    // patient payment----
+    app.post("/api/add-payment", async (req, res) => {
+      try {
+        const data = req.body;
+
+        const existingPayment = await paymentCollections.findOne({
+          transactionId: data.transactionId,
+        });
+
+        if (existingPayment) {
+          return res.status(409).json({
+            success: false,
+            message: "Payment already exists.",
+          });
+        }
+
+        const result = await paymentCollections.insertOne(data);
+
+        return res.status(201).json({
+          success: true,
+          message: "Payment added successfully.",
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        console.error("Add Payment Error:", error);
+
+        return res.status(500).json({
+          success: false,
+          message: "Internal Server Error",
+          error: error.message,
+        });
+      }
+    });
+
+    //patient get my payment---
+
+    app.get("/api/get-my-payments", async (req, res) => {
+      const id = req.query.patientId;
+
+      const filter = {
+        patientId: id,
+      };
+
+      const result = await paymentCollections.find(filter).toArray();
+      res.json(result);
+    });
     // ---Patient appointment area closed ----
 
-    //getting all doctors added schedules for find-doctors page--
+    //getting search and filter doctors added schedules for find-doctors page--
 
-    app.get("/api/all-schedules", async (req, res) => {
-      const result = await doctorScheduleCollections.find().toArray();
-      res.send(result);
+    app.get("/api/schedules", async (req, res) => {
+      const search = req.query.search
+      const specialization = req.query.specialization
+      const query = {};
+
+      if (search) {
+        query.name = {
+          $regex: search,
+          $options: "i",
+        };
+      }
+
+      if (specialization) {
+        query.specialization = specialization;
+      }
+
+
+      const result = await doctorScheduleCollections.find(query).toArray();
+
+      res.json(result);
     });
     // get single schedule for details page
     app.get("/api/single-schedule", async (req, res) => {
@@ -159,12 +244,10 @@ async function run() {
 
     // delete schedule by id---
     app.delete("/api/delete-schedule", async (req, res) => {
-
-      const id = req.query.scheduleId
+      const id = req.query.scheduleId;
       const filter = {
-        _id: new ObjectId(id)
-      }
-      
+        _id: new ObjectId(id),
+      };
 
       const result = await doctorScheduleCollections.deleteOne(filter);
       res.send(result);
@@ -218,16 +301,14 @@ async function run() {
 
     //for doctor data by doctorId - use to submit schedule with details--
 
-    app.get('/api/my/doctor', async(req,res)=>{
-        const query = {}
-        if(req.query.doctorId){
-            query.doctorId = req.query.doctorId
-        }
-        const result = await doctorCollections.findOne(query)
-        res.json(result)
-    })
-
-   
+    app.get("/api/my/doctor", async (req, res) => {
+      const query = {};
+      if (req.query.doctorId) {
+        query.doctorId = req.query.doctorId;
+      }
+      const result = await doctorCollections.findOne(query);
+      res.json(result);
+    });
 
     // adding schedule from schedule dashboard page
 
@@ -297,10 +378,47 @@ async function run() {
     // this is for patient post api
 
     app.post("/api/add-appointment", async (req, res) => {
-      const data = req.body;
-      const result = await appointmentCollections.insertOne(data);
-      res.send(result);
+      try {
+        const { sessionId } = req.body;
+
+        const existing = await appointmentCollections.findOne({
+          sessionId: sessionId,
+        });
+
+        if (existing) {
+          return res.status(409).json({
+            success: false,
+            message: "Appointment already exists",
+          });
+        }
+
+        const result = await appointmentCollections.insertOne(req.body);
+
+        res.status(201).json({
+          success: true,
+          insertedId: result.insertedId,
+        });
+      } catch (err) {
+        res.status(500).json({
+          success: false,
+          message: err.message,
+        });
+      }
     });
+
+    // app.post("/api/add-appointment", async (req, res) => {
+    //   const data = req.body;
+    //   const {sessionId} = data
+    //   const existing = await appointmentCollections.findOne({
+    //     sessionId,
+    //   })
+
+    //   if(!existing){
+
+    //     const result = await appointmentCollections.insertOne(data);
+    //     res.send(result);
+    //   }
+    // });
 
     // await client.db("admin").command({ ping: 1 });
     console.log(
